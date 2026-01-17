3f266d817260b94cdc6fe68ec1164f1070f2fe77
hazemxiii <101417956+hazemxiii@users.noreply.github.com>
2026-01-17 14:04:07 +0200

    ui/lives+time

--- src/games/memory_game/js/script.js
+++ src/games/memory_game/js/script.js
@@ -1,5 +1,27 @@
+var flipAllowed = true;
+var flippedCard = undefined;
+var images = [
+  "https://wallpapercave.com/wp/wp9583413.jpg",
+  "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2024/12/gojo-win.jpg?q=49&fit=crop&w=528&h=374&dpr=2",
+];
+let elapsedTime = document.getElementById("elapsed-time");
+let timeLeft = document.getElementById("time-left");
+let timeLimitSeconds = 30;
+var used = [];
+document.getElementById("start").addEventListener("click", () => {
+  buildGrid(5);
+});
+buildGrid(2);
+
+function buildInfo() {
+  if (timeLimitSeconds == 0) {
+    // TODO remove time left when there's no time limit
+  }
+}
+
 function buildGrid(size) {
-  let totalSpace = Math.min(window.innerHeight, window.innerWidth);
+  buildInfo();
+  let totalSpace = Math.min(window.innerHeight - 80, window.innerWidth);
   let availSpace = totalSpace - (size + 1) * 10;
   let cardSize = availSpace / size;
   let grid = document.getElementById("game-grid");
@@ -7,12 +29,7 @@ function buildGrid(size) {
   grid.style.height = totalSpace + "px";
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   for (let i = 0; i < size * size; i++) {
-    grid.appendChild(
-      buildCard(
-        cardSize,
-        "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2024/12/gojo-win.jpg?q=49&fit=crop&w=528&h=374&dpr=2"
-      )
-    );
+    grid.appendChild(buildCard(cardSize, getRandomImg()));
   }
   document.getElementById("pre-game").classList.add("playing");
   document.getElementById("game-play-ui").classList.add("playing");
@@ -42,15 +59,47 @@ function buildCard(cardSize, imgUrl) {
   card.appendChild(front);
   card.appendChild(back);
 
-  card.addEventListener("click", () => {
-    card.classList.add("flipped");
-  });
+  card.addEventListener("click", (e) => flipCard(card));
 
   return card;
 }
 
-document.getElementById("start").addEventListener("click", () => {
-  buildGrid(5);
-});
+function flipCard(card) {
+  if (!flipAllowed) {
+    return;
+  }
+  if (!card.classList.contains("flipped")) {
+    card.classList.add("flipped");
+    if (flippedCard == undefined) {
+      flippedCard = card;
+    } else {
+      flipAllowed = false;
+      setTimeout(() => {
+        if (getCardImg(flippedCard) == getCardImg(card)) {
+          flippedCard.classList.add("right");
+          card.classList.add("right");
+        } else {
+          flippedCard.classList.remove("flipped");
+          card.classList.remove("flipped");
+        }
+        flippedCard = undefined;
+        flipAllowed = true;
+      }, 1000);
+    }
+  }
+}
 
-buildGrid(5);
+function getRandomImg() {
+  let random = Math.round(Math.random() * (images.length - 1));
+  let img = images[random];
+  if (used.includes(img)) {
+    images = images.slice(0, random).concat(images.slice(random + 1));
+  } else {
+    used.push(img);
+  }
+  return img;
+}
+
+function getCardImg(card) {
+  return card.getElementsByTagName("img")[0].src;
+}
