464f98d2bcf7c763a0441938a28b433a28c7f5a3
hazemxiii <101417956+hazemxiii@users.noreply.github.com>
2026-01-18 21:03:22 +0200

    logic_without_settings

--- src/games/memory_game/js/script.js
+++ src/games/memory_game/js/script.js
@@ -1,44 +1,111 @@
 var flipAllowed = true;
 var flippedCard = undefined;
-var images = [
-  "https://wallpapercave.com/wp/wp9583413.jpg",
-  "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2024/12/gojo-win.jpg?q=49&fit=crop&w=528&h=374&dpr=2",
-];
-let elapsedTime = document.getElementById("elapsed-time");
-let timeLeft = document.getElementById("time-left");
-let timeLimitSeconds = 30;
-var used = [];
+var images;
+let userImgs;
+let timeLeftText = document.getElementById("time-left");
+let progressBar = document.querySelector(".progress span");
+let livesText = document.getElementById("lives-count");
+let timeLimitSeconds;
+let timeElapsed;
+let lives;
+let size;
+let pairs;
+let livesRemain;
+let timer;
+initGameSettings();
+buildSettings();
+// LISTENERS
 document.getElementById("start").addEventListener("click", () => {
-  buildGrid(5);
+  initGameSettings();
+  buildGrid(size);
 });
-buildGrid(2);
+document.getElementById("play-again").addEventListener("click", () => {
+  restartGame();
+});
 
+// BUILD UI
 function buildInfo() {
   if (timeLimitSeconds == 0) {
-    // TODO remove time left when there's no time limit
+    timeLeftText.style.display = "none";
+    progressBar.style.width = "100%";
+  } else {
+    progressBar.style.display = "block";
+    progressBar.style.width = "0%";
   }
+  livesText.textContent = `x${livesRemain}`;
 }
-
+function showEndDialog(isWin) {
+  let dialog = document.getElementById("game-end");
+  dialog.classList.remove("hide");
+  if (isWin) {
+    dialog.classList.add("win");
+    dialog
+      .querySelector("#game-end-icon-cont i")
+      .classList.remove("fa-face-frown");
+    dialog
+      .querySelector("#game-end-icon-cont i")
+      .classList.add("fa-face-smile");
+    dialog.querySelector("#game-end-title").textContent = "You Won!";
+    dialog.querySelector("#game-end-msg").textContent =
+      "Congratulations! You matched all pairs successfully.";
+  } else {
+    dialog.classList.remove("win");
+    dialog
+      .querySelector("#game-end-icon-cont i")
+      .classList.remove("fa-face-smile");
+    dialog
+      .querySelector("#game-end-icon-cont i")
+      .classList.add("fa-face-frown");
+    dialog.querySelector("#game-end-title").textContent = "Game Over";
+    dialog.querySelector("#game-end-msg").textContent = "Better Luck Next Time";
+  }
+  dialog.querySelector("#game-end-time .game-end-val").textContent =
+    toClock(timeElapsed);
+  dialog.querySelector("#game-end-mistakes .game-end-val").textContent =
+    lives - livesRemain;
+}
+function initGameSettings() {
+  document.getElementById("game-grid").innerHTML = "";
+  document.getElementById("game-end").classList.add("hide");
+  images = [
+    "https://wallpapercave.com/wp/wp9583413.jpg",
+    "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2024/12/gojo-win.jpg?q=49&fit=crop&w=528&h=374&dpr=2",
+  ];
+  userImgs = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxIgvIrw7Cdcd18NDXo9PX6oRpNo8H5q6_Hw&s",
+  ];
+  timeLimitSeconds = 50;
+  timeElapsed = 0;
+  lives = 2;
+  size = 2;
+  pairs = 0;
+  livesRemain = lives;
+  flipAllowed = true;
+  flippedCard = undefined;
+}
 function buildGrid(size) {
   buildInfo();
-  let totalSpace = Math.min(window.innerHeight - 80, window.innerWidth);
+  let totalSpace = Math.min(window.innerHeight - 80, window.innerWidth - 20);
   let availSpace = totalSpace - (size + 1) * 10;
   let cardSize = availSpace / size;
   let grid = document.getElementById("game-grid");
   grid.style.width = totalSpace + "px";
   grid.style.height = totalSpace + "px";
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  let imgs = getImgs();
   for (let i = 0; i < size * size; i++) {
-    grid.appendChild(buildCard(cardSize, getRandomImg()));
+    grid.appendChild(buildCard(cardSize, imgs[i]));
   }
   document.getElementById("pre-game").classList.add("playing");
   document.getElementById("game-play-ui").classList.add("playing");
   grid.scrollIntoView({ behavior: "smooth" });
+  setTimeout(() => {
+    startGame(tick);
+  }, 1000);
 }
-
 function buildCard(cardSize, imgUrl) {
   let card = document.createElement("div");
-  card.classList.add("card");
+  card.classList.add("card", "flipped");
   card.style.width = cardSize + "px";
   card.style.height = cardSize + "px";
   let front = document.createElement("div");
@@ -63,6 +130,25 @@ function buildCard(cardSize, imgUrl) {
 
   return card;
 }
+function buildSettings() {
+  buildGridSettings();
+}
+function buildGridSettings() {
+  document.querySelectorAll("#grid-options-row span").forEach((e) => {
+    if (e.getAttribute("value") == size) {
+      e.classList.add("selected");
+    } else {
+      e.classList.remove("selected");
+    }
+    e.addEventListener("click", () => {
+      document
+        .querySelector("#grid-options-row span.selected")
+        .classList.remove("selected");
+      e.classList.add("selected");
+    });
+  });
+}
+function buildTimeSettings() {}
 
 function flipCard(card) {
   if (!flipAllowed) {
@@ -73,33 +159,88 @@ function flipCard(card) {
     if (flippedCard == undefined) {
       flippedCard = card;
     } else {
-      flipAllowed = false;
-      setTimeout(() => {
-        if (getCardImg(flippedCard) == getCardImg(card)) {
-          flippedCard.classList.add("right");
-          card.classList.add("right");
-        } else {
-          flippedCard.classList.remove("flipped");
-          card.classList.remove("flipped");
+      if (getCardImg(flippedCard) == getCardImg(card)) {
+        flippedCard.classList.add("right");
+        card.classList.add("right");
+        pairs++;
+        if (pairs == (size * size) / 2) {
+          endGame(true);
         }
         flippedCard = undefined;
-        flipAllowed = true;
-      }, 1000);
+      } else {
+        flipAllowed = false;
+        livesRemain--;
+        livesText.textContent = `x${livesRemain}`;
+        if (livesRemain == 0) {
+          endGame(false);
+          return;
+        }
+        setTimeout(() => {
+          flippedCard.classList.remove("flipped");
+          card.classList.remove("flipped");
+          flippedCard = undefined;
+          flipAllowed = true;
+        }, 500);
+      }
     }
   }
 }
 
-function getRandomImg() {
-  let random = Math.round(Math.random() * (images.length - 1));
-  let img = images[random];
-  if (used.includes(img)) {
-    images = images.slice(0, random).concat(images.slice(random + 1));
-  } else {
-    used.push(img);
-  }
-  return img;
+// CONTROL GAME
+function startGame() {
+  tick();
+  document.querySelectorAll(".card").forEach((card) => {
+    card.classList.remove("flipped");
+  });
 }
 
+function endGame(isWin) {
+  clearInterval(timer);
+  flipAllowed = false;
+  showEndDialog(isWin);
+}
+
+function restartGame() {
+  initGameSettings();
+  buildGrid(size);
+}
+
+// HELPERS
+function tick() {
+  timer = setInterval(() => {
+    timeElapsed++;
+    document.querySelector("#elapsed-time .time").textContent =
+      toClock(timeElapsed);
+    timeLeftText.querySelector(".time").textContent = toClock(
+      timeLimitSeconds - timeElapsed,
+    );
+    progressBar.style.width = `${(timeElapsed / timeLimitSeconds) * 100}%`;
+    if (timeElapsed == timeLimitSeconds) {
+      endGame(false);
+    }
+  }, 1000);
+}
+
+function toClock(seconds) {
+  let minutes = Math.floor(seconds / 60);
+  let remainingSeconds = seconds % 60;
+  return `${minutes.toString().padStart(2, "0")}:${remainingSeconds.toString().padStart(2, "0")}`;
+}
+function getImgs() {
+  let imgsLeft = (size * size) / 2 - userImgs.length;
+  userImgs = shuffle(userImgs);
+  if (imgsLeft > 0) {
+    userImgs = userImgs.concat(shuffle(images).slice(0, imgsLeft));
+  }
+  return shuffle(userImgs.concat(userImgs));
+}
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
 function getCardImg(card) {
   return card.getElementsByTagName("img")[0].src;
 }
